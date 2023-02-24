785dda7efa9ee7b38401cbc50e5bb6801e14a6d4
Karthik A Bacherao <karthik.bacherao@gmail.com>
2023-02-24 15:52:36 -0800

    add handlers

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
+const logger = require('ololog'); 
 
+/*
+ * WhatsApp API Message Handler
+ */
 async function init() {
-	console.log("initializing...");
-	console.log("private rep...");
+	logger.green("initializing...");
 }
 
-module.exports = { init };
+/*
+ * Handle messages from Customers.
+ * Returns an object with a response type and response.
+ * Response: {
+ * 	 responseType: "text"
+ * 	 text: "text response",
+ * }
+ */
+async handleUserMessage (m) {
+
+	var response = null;
+
+	/*
+	 * On receiving a message, identify the type, intent 
+	 * and respond accordingly.
+	 */
+	logger.blue("message received from: ", m.from, " message: ", m);
+
+	// Respond to "hi" with "Hello, how are you?"
+	if (m.type == "text" &&
+		m.body == "hi") {
+
+		// received a greeting, respond with a greeting
+		response = {
+			responseType: "text",
+			text: "Hello, how are you?",
+		};
+	}
+
+	logger.blue("response: " + JSON.stringify(response));
+	return response;
+}
+
+/*
+ * Handle messages from Admins.
+ */
+async handleAdminMessage (m) {
+
+	var response = null;
+
+	logger.yellow("message received from admin: " + m);
+
+	return { responseType: "text", text: "Admin message received." };
+}
+
+module.exports =
+{
+	init,
+	handleCustomerMessage,
+	handleAdminMessage
+};
